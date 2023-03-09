d9be2ba8b63c722014a4c4bf8dac422660da8473
sanglv <lsang2884@gmail.com>
2023-03-09 23:34:30 +0700

    fix render types build

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+import Form from './packages/Form';
+import Input from './packages/Input';
+import Radio from './packages/Radio';
+import RadioGroup from './packages/RadioGroup';
+
+export {Input, Radio, RadioGroup};
+export default Form;
